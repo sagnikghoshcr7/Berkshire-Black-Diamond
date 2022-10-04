51e293d9fdee00c8286af36f0cf2079e33755771
Anish Dey <adanishdey59@gmail.com>
2022-10-04 23:16:46 +0530

    form input bug fix

---
+++ Client/src/form-input/form-input.component.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import {
+  GroupContainer,
+  FormInputContainer,
+  FormInputLabel
+} from './form-input.styles';
+
+const FormInput = ({ handleChange, label, ...props }) => (
+  <GroupContainer>
+    <FormInputContainer onChange={handleChange} {...props} />
+    {label ? (
+      <FormInputLabel className={props.value.length ? 'shrink' : ''}>
+        {label}
+      </FormInputLabel>
+    ) : null}
+  </GroupContainer>
+);
+
+export default FormInput;
