98dbbc0130a2be587c057ea7649d2f2c90a9d727
Soumya Roychowdhury <91222943+SoumyaRC1604@users.noreply.github.com>
2022-10-06 00:28:05 +0530

    update shop feat

---
+++ Server/shop/shop.component.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, lazy, Suspense } from 'react';
+import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
+
+import Spinner from '../../components/spinner/spinner.component';
+
+import { ShopPageContainer } from './shop.styles';
+
+const CollectionsOverviewContainer = lazy(() =>
+  import('../../components/collections-overview/collections-overview.container')
+);
+
+const CollectionPageContainer = lazy(() =>
+  import('../collection/collection.container')
+);
+
+export const ShopPage = ({ fetchCollectionsStart, match }) => {
+  useEffect(() => {
+    fetchCollectionsStart();
+  }, [fetchCollectionsStart]);
+
+  return (
+    <ShopPageContainer>
+      <Suspense fallback={<Spinner />}>
+        <Route
+          exact
+          path={`${match.path}`}
+          component={CollectionsOverviewContainer}
+        />
+        <Route
+          path={`${match.path}/:collectionId`}
+          component={CollectionPageContainer}
+        />
+      </Suspense>
+    </ShopPageContainer>
+  );
+};
+
+const mapDispatchToProps = dispatch => ({
+  fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(ShopPage);
