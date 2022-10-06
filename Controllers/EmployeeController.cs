85538c8ac2633e98c4e4bcb19cd84e56f774f38d
Sid's Windows <1906211@kiit.ac.in>
2022-10-06 11:15:33 +0530

    Added Controller

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Http.Description;
+using GenAPI.Models;
+
+namespace GenAPI.Controllers
+{
+    [EnableCors(origins: "http://localhost:4200", headers: "*", methods: "*")]
+    public class EmployeesController : ApiController
+    {
+        private TestEntities db = new TestEntities();
+
+        // GET: api/Employees
+        public IQueryable<Employee> GetEmployees()
+        {
+            return db.Employees;
+        }
+
+        // GET: api/Employees/5
+        [ResponseType(typeof(Employee))]
+        public IHttpActionResult GetEmployee(int id)
+        {
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(employee);
+        }
+
+        // PUT: api/Employees/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutEmployee(int id, Employee employee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != employee.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(employee).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployeeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Employees
+        [ResponseType(typeof(Employee))]
+        public IHttpActionResult PostEmployee(Employee employee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Employees.Add(employee);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = employee.Id }, employee);
+        }
+
+        // DELETE: api/Employees/5
+        [ResponseType(typeof(Employee))]
+        public IHttpActionResult DeleteEmployee(int id)
+        {
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+
+            return Ok(employee);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool EmployeeExists(int id)
+        {
+            return db.Employees.Count(e => e.Id == id) > 0;
+        }
+    }
+}
