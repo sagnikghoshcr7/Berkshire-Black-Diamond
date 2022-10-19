d685a337facbd9de40bf8ed750bd9032bb1415c4
Srestha Maitra <85587320+Srestha-Maitra@users.noreply.github.com>
2022-10-19 23:29:53 +0530

    Add files via upload

---
+++ Properties/cart/cart.tsx
@@ -0,0 +1,82 @@
+import formatPrice from 'utils/formatPrice';
+import CartProducts from './CartProducts';
+
+import { useCart } from 'contexts/cart-context';
+
+import * as S from './style';
+
+const Cart = () => {
+  const { products, total, isOpen, openCart, closeCart } = useCart();
+
+  const handleCheckout = () => {
+    if (total.productQuantity) {
+      alert(
+        `Checkout - Subtotal: ${total.currencyFormat} ${formatPrice(
+          total.totalPrice,
+          total.currencyId
+        )}`
+      );
+    } else {
+      alert('Add some product in the cart!');
+    }
+  };
+
+  const handleToggleCart = (isOpen: boolean) => () =>
+    isOpen ? closeCart() : openCart();
+
+  return (
+    <S.Container isOpen={isOpen}>
+      <S.CartButton onClick={handleToggleCart(isOpen)}>
+        {isOpen ? (
+          <span>X</span>
+        ) : (
+          <S.CartIcon>
+            <S.CartQuantity title="Products in cart quantity">
+              {total.productQuantity}
+            </S.CartQuantity>
+          </S.CartIcon>
+        )}
+      </S.CartButton>
+
+      {isOpen && (
+        <S.CartContent>
+          <S.CartContentHeader>
+            <S.CartIcon large>
+              <S.CartQuantity>{total.productQuantity}</S.CartQuantity>
+            </S.CartIcon>
+            <S.HeaderTitle>Cart</S.HeaderTitle>
+          </S.CartContentHeader>
+
+          <CartProducts products={products} />
+
+          <S.CartFooter>
+            <S.Sub>SUBTOTAL</S.Sub>
+            <S.SubPrice>
+              <S.SubPriceValue>{`${total.currencyFormat} ${formatPrice(
+                total.totalPrice,
+                total.currencyId
+              )}`}</S.SubPriceValue>
+              <S.SubPriceInstallment>
+                {total.installments ? (
+                  <span>
+                    {`OR UP TO ${total.installments} x ${
+                      total.currencyFormat
+                    } ${formatPrice(
+                      total.totalPrice / total.installments,
+                      total.currencyId
+                    )}`}
+                  </span>
+                ) : null}
+              </S.SubPriceInstallment>
+            </S.SubPrice>
+            <S.CheckoutButton onClick={handleCheckout} autoFocus>
+              Checkout
+            </S.CheckoutButton>
+          </S.CartFooter>
+        </S.CartContent>
+      )}
+    </S.Container>
+  );
+};
+
+export default Cart;
